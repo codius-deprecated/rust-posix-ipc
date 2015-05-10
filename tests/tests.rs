3c01bf5f989cdb9422a4b02c53a78fc7c8adeb92
Aidan Hobson Sayers <aidanhs@cantab.net>
2015-05-10 18:45:53 +0100

    rustup + attempt to fix test racyness

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,18 +1,28 @@
-extern crate "posix-ipc" as ipc;
+#![feature(std_misc)]
+extern crate posix_ipc as ipc;
 
 use ipc::signals;
+use std::thread;
+use std::sync::{Arc, Semaphore, Mutex};
 
 #[test]
 fn raise_and_catch_with_closure() {
-    let mut caught = false;
+    let mut caught: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+    let mut sem: Arc<Semaphore> = Arc::new(Semaphore::new(1));
+    sem.acquire();
     {
-        let f = |Signal| {caught = true;println!("Caught!")};
+        let sem = sem.clone();
+        let caught = caught.clone();
+        let f = move |s: signals::Signal| {
+            *caught.lock().unwrap() = true; sem.release()
+        };
         unsafe {
-            signals::Signal::Usr1.handle(Box::new(f));
+            signals::Signal::Usr1.handle(Box::new(f)).unwrap();
         }
     }
-    signals::Signal::Usr1.raise();
-    assert!(unsafe { caught });
+    signals::Signal::Usr1.raise().unwrap();
+    sem.acquire();
+    assert!(*caught.lock().unwrap());
 }
 
 #[test]
@@ -21,9 +31,10 @@ fn raise_and_catch_with_func() {
     {
         fn f(s: signals::Signal) {unsafe { caught = true }}
         unsafe {
-            signals::Signal::Usr1.handle(Box::new(f));
+            signals::Signal::Usr1.handle(Box::new(f)).unwrap();
         }
     }
-    signals::Signal::Usr1.raise();
+    signals::Signal::Usr1.raise().unwrap();
+    thread::sleep_ms(500); // this is really racy :(
     assert!(unsafe { caught });
 }
